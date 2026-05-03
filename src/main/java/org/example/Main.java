275c1d21164c7fa37b461066511ab9e82b9dbca7
Anna <anna@gmail.com>
2026-05-04 01:38:53 +0300

    finalized project, add BookingService, demo Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,17 @@
 package org.example;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Optional;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        BookingService bookingService = new BookingService();
+        TableService tableService = new TableService();
+        Customer customer = new Customer("c1", "Anna", "+380675894673");
+        Table table = new Table("t1",25,true);
+        Booking booking = bookingService.createBooking("b1", customer, table, Status.CREATED);
+        bookingService.confirmBooking("b1");
+        bookingService.findAllConfimBooking();
+        List<Booking> bookings = new ArrayList<>();
+        System.out.println();
     }
 }
